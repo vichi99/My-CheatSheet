58246dacd55a5e5416787afffa8f91b1e808fb72
Jan Vícha <john.vicha@seznam.cz>
2021-07-23 23:31:30 +0200

    update

--- cheatsheets/postgres.sh
+++ cheatsheets/postgres.sh
@@ -1,6 +1,9 @@
 # source: 
 # https://blog.timescale.com/tutorials/how-to-install-psql-on-mac-ubuntu-debian-windows/
 
+# install psql...
+sudo apt-get install postgresql-client
+
 # login
 psql -h [HOSTNAME] -p [PORT] -U [USERNAME] -W -d [DATABASENAME]
 
